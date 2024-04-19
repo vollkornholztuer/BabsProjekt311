808dbfbcff9d86f5f47067e8ceb6251916573825
Funzyy <103183666+Funzyy@users.noreply.github.com>
2024-04-19 14:18:15 +0200

    pinch wip

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import tkinter as tk
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
 cap = cv2.VideoCapture(0)
 
 # Function to calculate distance
+# p = x, y of landmark
+# sqrt((x2 - x1)^2 + (y2 - y1)^2) distance between 2 points
 def calculate_distance(p1, p2):
     return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
@@ -56,31 +59,33 @@ while True:
                         y0 = int(hand_landmarks.landmark[0].y * frame.shape[0])
                         cv2.line(frame, (x1, y1), (x0, y0), (0, 255, 0), 2)
 
-            # Detect pinch between thumb tip (4) and index finger tip (8)
+            # Detect pinch
             thumb_tip = landmarks_list[4]
             index_tip = landmarks_list[8]
-            if calculate_distance(thumb_tip, index_tip) < 50:  # Threshold for pinch
+            if calculate_distance(thumb_tip, index_tip) < 100:  # Threshold for pinch
                 is_pinching = True
+                # calc middle point between both tips
                 dragging_point = ((thumb_tip[0] + index_tip[0]) // 2, (thumb_tip[1] + index_tip[1]) // 2)
-                cv2.circle(frame, dragging_point, 10, (0, 255, 0), -1)  # Visual feedback for pinching
+                cv2.circle(frame, dragging_point, 10, (255, 255, 0), 2)  # Visual feedback for pinching
             else:
                 is_pinching = False
 
-             # Drag functionality
-            if is_pinching and dragging_point:
-                # Here you can update the position of the dragged object using `dragging_point`
-                cv2.circle(frame, dragging_point, 10, (255, 0, 0), -1)  # Visual feedback for dragging
+            # Drag 
+            # if is_pinching and dragging_point:
+                # here code to drag something
 
 
             # 13 and 17
             cv2.line(frame, (landmarks_list[13][0], landmarks_list[13][1]), (landmarks_list[17][0], landmarks_list[17][1]), (0, 255, 0), 2)
-            # 5 und 9
+            # 5 and 9
             cv2.line(frame, (landmarks_list[5][0], landmarks_list[5][1]), (landmarks_list[9][0], landmarks_list[9][1]), (0, 255, 0), 2)
-            # 9 und 13
+            # 9 and 13
             cv2.line(frame, (landmarks_list[9][0], landmarks_list[9][1]), (landmarks_list[13][0], landmarks_list[13][1]), (0, 255, 0), 2)
 
 
-    cv2.imshow('Frame', frame)
+    frame = cv2.resize(frame, (1280, 720))
+    
+    cv2.imshow('Webcam Feed', frame)
 
 
     # Check if the user pressed ESC and break the loop
@@ -89,7 +94,7 @@ while True:
         break
 
     # Check if the user closed the window and break the loop
-    if cv2.getWindowProperty('Frame', cv2.WND_PROP_VISIBLE) < 1:
+    if cv2.getWindowProperty('Webcam Feed', cv2.WND_PROP_VISIBLE) < 1:
         break
 
 
