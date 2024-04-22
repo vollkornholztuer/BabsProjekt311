3c96f835ca8658a3da0476009106c0e5acda6c4b
Funzyy <Marcellilie@gmx.de>
2024-04-22 23:38:20 +0200

    detection with threshold wip

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_c
 cap = cv2.VideoCapture(0)
 
 # ingertips for clarity
-# middle_tip = landmarks_list[12]
 # wrist = landmarks_list[0]
-# pinky_tip = landmarks_list[20]
-# ring_tip = landmarks_list[16]
 # thumb_tip = landmarks_list[4]
 # index_tip = landmarks_list[8]
+# middle_tip = landmarks_list[12]
+# ring_tip = landmarks_list[16]
+# pinky_tip = landmarks_list[20]
 
 # Hand middle point 
 def calculate_palm_points(landmarks_list):
@@ -26,29 +26,35 @@ def calculate_palm_points(landmarks_list):
 
     return np.mean([wrist, thumb_base, index_base, middle_base, ring_base, pinky_base], axis=0)
 
-
 # Function to calculate distance
 def calculate_distance(p1, p2):
-    # p = x, y of landmark
-    # sqrt((x2 - x1)^2 + (y2 - y1)^2) distance between 2 points
+#     # p = x, y of landmark
+#     # sqrt((x2 - x1)^2 + (y2 - y1)^2) distance between 2 points
     return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
-def detect_is_finger_down(landmarks_list):
+def calculate_hand_size(landmarks_list, palm_center):
+    return calculate_distance(landmarks_list[0], palm_center)
+
+def normalized_distance(p1, p2, hand_size):
+    raw_distance = calculate_distance(p1, p2)
+    return raw_distance / hand_size
+
+def detect_is_finger_down(landmarks_list, hand_size):
     finger_status = {
-        'thumb_down': calculate_distance(landmarks_list[4], landmarks_list[0]) < 0.5,
-        'index_down': calculate_distance(landmarks_list[8], landmarks_list[0]) < 0.5,
-        'middle_down': calculate_distance(landmarks_list[12], landmarks_list[0]) < 0.5,
-        'ring_down': calculate_distance(landmarks_list[16], landmarks_list[0]) < 0.5,
-        'pinky_down': calculate_distance(landmarks_list[20], landmarks_list[0]) < 0.5
+        # 'thumb_down': normalized_distance(landmarks_list[4], landmarks_list[0], hand_size) < 0.25,
+        'index_down': normalized_distance(landmarks_list[8], landmarks_list[0], hand_size) < 1.5,
+        'middle_down': normalized_distance(landmarks_list[12], landmarks_list[0], hand_size) < 1.5,
+        'ring_down': normalized_distance(landmarks_list[16], landmarks_list[0], hand_size) < 1.5,
+        'pinky_down': normalized_distance(landmarks_list[20], landmarks_list[0], hand_size) < 1.5
     }
     return finger_status
 
-def detect_pinch(landmarks_list):
-    if calculate_distance(landmarks_list[4], landmarks_list[8]) < 100 and not detect_is_finger_down(landmarks_list)['thumb_down'] and not detect_is_finger_down(landmarks_list)['index_down']:  # Threshold for pinch
+def detect_pinch(landmarks_list, hand_size):
+    if normalized_distance(landmarks_list[4], landmarks_list[8], hand_size) < 0.25 and not detect_is_finger_down(landmarks_list, hand_size)['index_down']:
         is_pinching = True
         # calc middle point between both tips
         dragging_point = ((landmarks_list[4][0] + landmarks_list[8][0]) // 2, (landmarks_list[4][1] + landmarks_list[8][1]) // 2)
-        cv2.circle(frame, dragging_point, 10, (0, 0, 0), 2)  # Visual feedback for pinching
+        cv2.circle(frame, dragging_point, 10, (255, 255, 255), 2)  # Visual feedback for pinching
     else:
         is_pinching = False
 
@@ -59,14 +65,15 @@ is_pinching = False
 # don't be mean
 fu = False
 
+# show lines
+show = True
+
 while True:
     # Read the frame
     ret, frame = cap.read()
 
-
     # frame to RGB
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
 
     # Mediapipe hands
     results = hands.process(frame_rgb)
@@ -79,43 +86,61 @@ while True:
                 if i <= 20: 
                     x1 = int(landmark.x * frame.shape[1]) 
                     y1 = int(landmark.y * frame.shape[0])
+                    # z1 = (landmark.z * frame.shape[2])
                     
                     landmarks_list.append((x1, y1))
                     
-                    cv2.circle(frame, (x1, y1), 5, (0, 0, 255), -1)
-                    
-                    cv2.putText(frame, str(i), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
                     
                     if i < 20:
                         landmark_next = hand_landmarks.landmark[i+1] # Get the next landmark
                     
                     x2 = int(landmark_next.x * frame.shape[1])
                     y2 = int(landmark_next.y * frame.shape[0])
+                    # z2 = int(landmark_next.z * frame.shape[2])
                     
-                    if i%4 != 0:
-                        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) # Draw a line between the landmarks
-                        
-                    if i == 1 or i == 5 or i == 17:
-                        x0 = int(hand_landmarks.landmark[0].x * frame.shape[1])
-                        y0 = int(hand_landmarks.landmark[0].y * frame.shape[0])
-                        cv2.line(frame, (x1, y1), (x0, y0), (0, 255, 0), 2)
+                    if show:
+                        cv2.circle(frame, (x1, y1), 5, (0, 0, 255), -1)
+                    
+                        cv2.putText(frame, str(i), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
-            # 13 and 17
-            cv2.line(frame, (landmarks_list[13][0], landmarks_list[13][1]), (landmarks_list[17][0], landmarks_list[17][1]), (0, 255, 0), 2)
-            # 5 and 9
-            cv2.line(frame, (landmarks_list[5][0], landmarks_list[5][1]), (landmarks_list[9][0], landmarks_list[9][1]), (0, 255, 0), 2)
-            # 9 and 13
-            cv2.line(frame, (landmarks_list[9][0], landmarks_list[9][1]), (landmarks_list[13][0], landmarks_list[13][1]), (0, 255, 0), 2)
+                        if i%4 != 0:
+                            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) # Draw a line between the landmarks
+                            
+                        if i == 1 or i == 5 or i == 17:
+                            x0 = int(hand_landmarks.landmark[0].x * frame.shape[1])
+                            y0 = int(hand_landmarks.landmark[0].y * frame.shape[0])
+                            cv2.line(frame, (x1, y1), (x0, y0), (0, 255, 0), 2)
+            if show:
+                # 13 and 17
+                cv2.line(frame, (landmarks_list[13][0], landmarks_list[13][1]), (landmarks_list[17][0], landmarks_list[17][1]), (0, 255, 0), 2)
+                # 5 and 9
+                cv2.line(frame, (landmarks_list[5][0], landmarks_list[5][1]), (landmarks_list[9][0], landmarks_list[9][1]), (0, 255, 0), 2)
+                # 9 and 13
+                cv2.line(frame, (landmarks_list[9][0], landmarks_list[9][1]), (landmarks_list[13][0], landmarks_list[13][1]), (0, 255, 0), 2)
 
             palm_center = calculate_palm_points(landmarks_list)
-            cv2.circle(frame, (int(palm_center[0]), int(palm_center[1])), 15, (255, 255, 255), 2)  # Visual feedback for palm center
+            # cv2.circle(frame, (int(palm_center[0]), int(palm_center[1])), 15, (255, 255, 255), 2)  # Visual feedback for palm center
 
-            # detect_pinch(landmarks_list)
+            hand_size = calculate_hand_size(landmarks_list, palm_center)
 
-            # print(calculate_distance(landmarks_list[12], landmarks_list[0]))
-            # if detect_is_finger_down(landmarks_list)['pinky_down'] and detect_is_finger_down(landmarks_list)['ring_down'] and detect_is_finger_down(landmarks_list)['index_down']:
-            if landmarks_list[12][1] > int(palm_center[1]) and landmarks_list[8][1] < int(palm_center[1]) and landmarks_list[16][1] < int(palm_center[1]) and landmarks_list[20][1] < int(palm_center[1]):
-                # fu = True
+            detect_pinch(landmarks_list, hand_size)
+
+
+            key = cv2.waitKey(1)
+            if key == ord('p'):
+
+                print("\n")
+                print("thumb: ",normalized_distance(landmarks_list[4], landmarks_list[0], hand_size))
+                print("index: ",normalized_distance(landmarks_list[8], landmarks_list[0], hand_size))
+                print("middle: ",normalized_distance(landmarks_list[12], landmarks_list[0], hand_size))
+                print("ring: ",normalized_distance(landmarks_list[16], landmarks_list[0], hand_size))
+                print("pinky: ",normalized_distance(landmarks_list[20], landmarks_list[0], hand_size))
+                print("\n")
+
+            # don't be mean
+            if detect_is_finger_down(landmarks_list, hand_size)['index_down'] and not detect_is_finger_down(landmarks_list, hand_size)['middle_down'] and detect_is_finger_down(landmarks_list, hand_size)['ring_down'] and detect_is_finger_down(landmarks_list, hand_size)['pinky_down']: 
+                fu = True
                 print("Don't be mean!")
 
 
