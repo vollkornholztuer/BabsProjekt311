bab4d575ac50a302e26f78c8e6aacef0270ce3a5
Monnitz <moritz@broedla.de>
2024-04-24 14:39:59 +0200

    second window

--- show.py
+++ show.py
@@ -15,7 +15,8 @@ def showImage():
         ret, frame = cap.read()
         frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
         cv2.imshow('Input', frame)
-
+        cv2.imshow("second Input", frame)
+        
         c = cv2.waitKey(1)
         if c == 27:
             break
